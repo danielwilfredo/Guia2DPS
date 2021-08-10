5a2da67fc563e9a42643e06c1121c983843ef3c8
danielwilfredo <xvwilito11@gmail.com>
2021-08-09 22:21:04 -0600

    empezando ejercicio complementario

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import Form from "./components/Form"
 const App = () => {
+
+  const [tipo, setTipo] = useState(1);
+  
   return (
-    <div className="App">
+<>
+<div className="App">
       <div className="App-content">
-        <p>Aquí haremos nuestro TO-DO list</p>
-        <Form/>
+        <p>Seleccione una Opcion:</p>
+        <select onChange={e => setTipo(e.target.value)} value={tipo}>
+          <option value={1}>Ejemplo Guia 2</option>
+          <option value={2}>Ejercicio Complementario</option>
+        </select>
+        
+
+        {(tipo==1) ? <Form/> : <p>Selecciono Ejercicio Complementario</p>}
+
+
+
+
       </div>
     </div>
+</>
+
+
   );
 };
 export default App;
